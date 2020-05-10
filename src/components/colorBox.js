a7a6e5266ca6673863c3893ebb7e44aa74c7f7da
fortu-42 <fortunatoherrerap@gmail.com>
2020-05-10 02:52:32 -0400

    feat: added tailwind and implemented pagination

--- src/components/colorBox.js
+++ src/components/colorBox.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const ColorBox = ({ color }) => {
-  return <h1>{color.name}</h1>;
+  // console.log(color);
+  const stylesDecidedByProps = {
+    backgroundColor: color.color,
+  };
+
+  return (
+    <div className='h-1/3 lg:w-1/3' style={stylesDecidedByProps}>
+      <h1>{color.name}</h1>
+      <p>{color.color}</p>
+      <p>{color.year}</p>
+      <p>{color.pantone_value}</p>
+      <p>{color.name}</p>
+    </div>
+  );
 };
 
 export default ColorBox;
--- src/components/pages/colors.js
+++ src/components/pages/colors.js
@@ -3,25 +3,59 @@ import { HTTP } from '../../helpers/htttp-common';
 import ColorBox from '../colorBox';
 
 const Colors = () => {
-  const [pagination, setPagination] = useState({});
+  const [page, setPage] = useState(1);
+  const [pagination, setPagination] = useState({
+    perPage: 9,
+    total: 12,
+    totalPages: 2,
+  });
   const [colors, setColors] = useState([]);
   useEffect(() => {
-    HTTP.get('/colors', { params: pagination.page }).then(({ data }) => {
-      //   console.log(response.data);
+    const fetchColors = () => {
+      HTTP.get('/colors', { params: { page, per_page: 9 } }).then(
+        ({ data: { data, per_page, total, total_pages } }) => {
+          // console.log(data);
+          setColors(data);
+          setPagination({
+            perPage: per_page,
+            total,
+            totalPages: total_pages,
+          });
+        },
+      );
+    };
+    fetchColors();
+  }, [page]);
 
-      setColors(data.data);
-      setPagination({
-        page: data.page,
-        perPage: data.per_page,
-        totalItems: data.total,
-        totalPages: data.total_pages,
-      });
-    });
-  }, [pagination.page]);
+  const handleNext = () => {
+    if (page < pagination.totalPages) {
+      setPage(page + 1);
+    }
+  };
 
-  return colors.map((color, i) => {
-    return <ColorBox key={i} color={color} />;
-  });
+  const handlePrev = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+    return;
+  };
+
+  return (
+    <div className='flex flex-col h-full'>
+      <div className='text-2xl text-center py-4 border-solid border rounded border-gray-600 bg-gray-300'>
+        Colores
+      </div>
+      <div className='flex w-full h-full flex-grow flex-wrap'>
+        {colors.map((color, i) => {
+          return <ColorBox key={i} color={color} />;
+        })}
+      </div>
+      <div className='text-lg flex justify-between px-12 text-center py-4 border-solid border rounded border-gray-600 bg-gray-300'>
+        <button onClick={handlePrev}>Previous</button>
+        <button onClick={handleNext}>Next</button>
+      </div>
+    </div>
+  );
 };
 
 export default Colors;
